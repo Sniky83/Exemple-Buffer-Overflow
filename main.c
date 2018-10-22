ef28cdbe1ca7dceacbb650b4eaef368cc27e7a76
Sniky83 <sniky83@hotmail.fr>
2018-10-22 19:46:46 +0200

    Init commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void RentrerPrenom()
+{
+        char prenom[5];
+        printf("\nRentrez un prenom: ");
+        scanf("%s", &prenom); // %s pour les chaines, %c pour un char.
+        printf("Prenom: %s \n\n", prenom); // %s pour les chaines, %c pour un char.
+}
+
+int main()
+{
+    char choix;
+
+    do {
+        RentrerPrenom();
+
+        printf("Voulez vous quitter ?: ");
+        scanf("%s", &choix);
+        system("cls");
+    } while(choix != 'o');
+    return 0;
+}
